b1f2d5c849c135ad54f030d0be89ae2be35ce138
Carolyn Zhang <czhang92@gmail.com>
2013-11-17 01:06:44 -0500

    Basic demo for pace/break set works

--- UI_Controller/UI_Controller.ino
+++ UI_Controller/UI_Controller.ino
@@ -4,6 +4,18 @@
    Author: Ben Potash
    Date: 11/7/13 */
 //------------------------------------------------------
+#include <Adafruit_GFX.h>
+#include <Adafruit_SharpMem.h>
+#include <avr/pgmspace.h>
+
+
+// any pins can be used
+#define SCK 52 // yellow wire, clk
+#define MOSI 51 // brownish, same as DI (data in)
+#define SS 53 //gray, same as CS
+
+Adafruit_SharpMem display(SCK, MOSI, SS);
+
 int powerButton;
 int startButton;
 int diode;
@@ -15,60 +27,60 @@ int Decrease;
 int Increase;
 int Before;
 int Next;
+int currentScreen;
+int totalSec;
+
 
 //------------------------------------------------------
-void setup() {
-  selectButton = 51;
-  powerButton = 52;
-  startButton = A2;
-  diode = 53;
-  Select = A8;
-  Decrease = A9;
-  Increase = A10;
-  Before = A11;
-  Next = A12;
+void setup(void) {
+//  selectButton = 51;
+//  powerButton = 52;
+//  startButton = A2;
+//  diode = 53;
+  Select = A0;
+  Decrease = A1;
+  Increase = A2;
+  Before = A3;
+  Next = A4;
+  
+  currentScreen = 0;
 
   x_val = 0;
   y_val = 0;
   
-  pinMode(powerButton, INPUT);
-  pinMode(selectButton, INPUT);
-  pinMode(startButton, INPUT);
-  pinMode(A0,INPUT);
-  pinMode(A1,INPUT);
+//  pinMode(powerButton, INPUT);
+//  pinMode(selectButton, INPUT);
+//  pinMode(startButton, INPUT);
+//  pinMode(A0,INPUT);
+//  pinMode(A1,INPUT);
   pinMode(Select, INPUT);
   pinMode(Decrease, INPUT);
   pinMode(Increase, INPUT);
   pinMode(Before, INPUT);
   pinMode(Next, INPUT);
 
-  pinMode(diode, OUTPUT);
- 
-  digitalWrite(diode,LOW);
-  
+//  pinMode(diode, OUTPUT);
+// 
+//  digitalWrite(diode,LOW);
   Serial.begin(9600);
+  display.begin();
+  display.clearDisplay();
+  display.renderScreenPace();
+  totalSec = display.getPaceMin() * 60 + display.getPaceSec();
+  
+
 }
 //------------------------------------------------------
 void loop(){
-  x_val = analogRead(A0);
-  y_val = analogRead(A1);
+//  x_val = analogRead(A0);
+//  y_val = analogRead(A1);
   
   printOutputs();
-  turnDiodeOnOff();
-  
-  delay(100);
+//  turnDiodeOnOff();
+  usageTest();
 }
 //------------------------------------------------------
 void printOutputs(){
-//  Serial.print(digitalRead(powerButton));
-//  Serial.print(" ");
-//  Serial.print(analogRead(startButton));
-//  Serial.print(" ");
-//  Serial.print(x_val);
-//  Serial.print(" ");
-//  Serial.print(y_val);
-//  Serial.print(" ");
-//  Serial.println(digitalRead(selectButton));
   Serial.print(analogRead(Select));
   Serial.print(" ");
   Serial.print(analogRead(Decrease));
@@ -80,10 +92,56 @@ void printOutputs(){
   Serial.println(analogRead(Next));
 }
 //------------------------------------------------------
-void turnDiodeOnOff(){
-  if(digitalRead(powerButton)){
-    digitalWrite(diode,HIGH);}
-  else{
-    digitalWrite(diode,LOW);}
-}
+//void turnDiodeOnOff(){
+//  if(digitalRead(powerButton)){
+//    digitalWrite(diode,HIGH);}
+//  else{
+//    digitalWrite(diode,LOW);}
+//}
 //------------------------------------------------------
+
+void usageTest(){
+  if((analogRead(Select) == 1023) || (analogRead(Next) == 1023)){
+    //if current screen is pace screen
+    if(currentScreen == 0){
+      display.setPaceMin(totalSec / 60);
+      display.setPaceSec(totalSec % 60);
+      totalSec = display.getBreakMin() * 60 + display.getBreakSec();
+      display.renderScreenBreak();
+      currentScreen = 1;
+    } else {
+      display.setBreakMin(totalSec / 60);
+      display.setBreakSec(totalSec % 60);
+      totalSec = display.getPaceMin() * 60 + display.getPaceSec();
+      display.renderScreenPace();
+      currentScreen = 0;
+    }
+  }
+  
+  if (analogRead(Before) == 1023){
+    if(currentScreen == 0){
+      display.setPaceMin(totalSec / 60);
+      display.setPaceSec(totalSec % 60);
+      totalSec = display.getBreakMin() * 60 + display.getBreakSec();
+      display.renderScreenBreak();
+      currentScreen = 1;
+    } else {
+      display.setBreakMin(totalSec / 60);
+      display.setBreakSec(totalSec % 60);
+      totalSec = display.getPaceMin() * 60 + display.getPaceSec();
+      display.renderScreenPace();
+      currentScreen = 0;
+    }
+  }
+  
+  if((analogRead(Increase) == 1023)){
+    totalSec++;
+    display.renderTime(totalSec / 60, totalSec % 60);
+  }
+  
+  if((analogRead(Decrease) == 1023)){
+    totalSec--;
+    display.renderTime(totalSec / 60, totalSec % 60);
+  }
+}
+
